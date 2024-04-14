540fac9f6f1708db300e8d253d64960ca8ff4fca
Anton <a.chertok@akatech.com.ua>
2024-04-14 12:33:27 +0100

    feat: config tw and common styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,44 @@
 /** @type {import('tailwindcss').Config} */
+
+export const breakpoints = {
+  mobile: 390,
+  tablet: 768,
+  desktop: 1440,
+};
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/styles/**/*.{js,ts,jsx,tsx,css}",
   ],
+
   theme: {
-    extend: {},
+    screens: {
+      xs: `${breakpoints.mobile}px`,
+      md: `${breakpoints.tablet}px`,
+      xl: `${breakpoints.desktop}px`,
+    },
+
+    extend: {
+      colors: {
+        black: "#101828",
+        white: "#FFFFFF",
+        dark: "#475467",
+        accent: "#E44848",
+        "accent-hovered": "#E44848",
+        yellow: "#FFC531",
+        grey: "#F2F4F7",
+        intput: "#F7F7F7",
+      },
+
+      textColor: {},
+
+      backgroundColor: {},
+
+      borderColor: {},
+    },
   },
   plugins: [],
-}
-
+};
