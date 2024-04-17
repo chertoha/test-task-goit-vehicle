00414b0259cd5e92ee33461286a34fabeae53a3a
Anton <a.chertok@akatech.com.ua>
2024-04-17 20:31:52 +0100

    Update advertsApi.ts

--- src/redux/adverts/advertsApi.ts
+++ src/redux/adverts/advertsApi.ts
@@ -5,6 +5,15 @@ import { DEFAULT_QUERY_LIMIT, DEFAULT_QUERY_PAGE } from "services/config";
 
 type AdvertsQuery = {
   page: number;
+  search: string;
+  form: string;
+  options: unknown;
+};
+
+type AdvertsCountQuery = {
+  search: string;
+  form: string;
+  options: unknown;
 };
 
 export const advertsApi = createApi({
@@ -14,16 +23,32 @@ export const advertsApi = createApi({
 
   endpoints: builder => ({
     getAdverts: builder.query<Vehicle[], AdvertsQuery>({
-      query: ({ page = DEFAULT_QUERY_PAGE }) => ({
+      query: ({ page = DEFAULT_QUERY_PAGE, options, ...rest }) => ({
         url: "/advert",
         method: "GET",
         params: {
           page,
           limit: DEFAULT_QUERY_LIMIT,
+          ...rest,
         },
       }),
+    }),
+
+    getAdvertsCount: builder.query<number, AdvertsCountQuery>({
+      query: ({ options, ...rest }) => ({
+        url: "/advert",
+        method: "GET",
+        params: {
+          ...rest,
+        },
+      }),
+      transformResponse: (response: Vehicle[]) => response.length,
     }),
   }),
 });
 
-export const { useGetAdvertsQuery } = advertsApi;
+export const {
+  useGetAdvertsQuery,
+  useLazyGetAdvertsQuery,
+  useGetAdvertsCountQuery,
+} = advertsApi;
