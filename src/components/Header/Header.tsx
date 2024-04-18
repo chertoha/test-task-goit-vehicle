f8c4a305867bba877fa40201d6605c8189915daf
Anton <a.chertok@akatech.com.ua>
2024-04-18 21:04:02 +0100

    feat: header + header-mob

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,9 +3,9 @@ import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="py-5">
-      <div className="container-default outline outline-orange-400">
-        <div className="flex">
+    <header className="pt-8">
+      <div className="container-default">
+        {/* <div className="flex">
           <NavLink
             className="nav"
             to={ROUTES.HOME}
@@ -24,7 +24,37 @@ const Header = () => {
           >
             Favorites
           </NavLink>
-        </div>
+        </div> */}
+        <nav>
+          <ul className="flex gap-x-10 border-b border-black/20  max-md:gap-x-3">
+            <li>
+              <NavLink
+                className="block relative heading-2 pb-6 after:hidden after:absolute after:left-0 after:bottom-[-3px] after:h-[3px] md:after:h-[5px] after:w-full after:bg-accent  [&.active]:after:block"
+                to={ROUTES.HOME}
+              >
+                Home
+              </NavLink>
+            </li>
+
+            <li>
+              <NavLink
+                className="block relative heading-2 pb-6 after:hidden after:absolute after:left-0 after:bottom-[-3px] after:h-[3px] md:after:h-[5px] after:w-full after:bg-accent  [&.active]:after:block"
+                to={ROUTES.CATALOG}
+              >
+                Catalog
+              </NavLink>
+            </li>
+
+            <li>
+              <NavLink
+                className="block relative heading-2 pb-6 after:hidden after:absolute after:left-0 after:bottom-[-3px]  after:h-[3px] md:after:h-[5px] after:w-full after:bg-accent  [&.active]:after:block"
+                to={ROUTES.FAVORITES}
+              >
+                Favorites
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
       </div>
     </header>
   );
