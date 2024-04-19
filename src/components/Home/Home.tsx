07e00437fda89c5a640e952e8b36a7c7ead4f7a6
Anton <a.chertok@akatech.com.ua>
2024-04-19 23:46:41 +0100

    Update Home.tsx

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,9 +2,117 @@ import VisitCard from "components/VisitCard";
 
 const Home = () => {
   return (
-    <div className="bg-[#292e34] h-screen ">
-      <div className="container-default">
+    <div className="bg-[#292e34] h-screen relative overflow-hidden">
+      <div className="container-default relative z-10">
         <VisitCard />
+      </div>
+
+      <div className="absolute top-[-100px] z-0 grid grid-cols-2 gap-8  -rotate-12 text-white/10">
+        <div>
+          <h2 className="heading-2 mt-3">Campervan and RV Rental</h2>
+          <p className="body-1 leading-loose">
+            Although campervanning is popular throughout the world, nowhere is it more engrained in
+            the culture than in the United States! The US is the perfect place to rent a campervan
+            or RV and hit the road. The idea of the iconic American road trips still attracts people
+            from far and wide, all wanting to experience the splendor of its many landscapes.
+            Curiosity draws in local and international crowds to all the wonders that can be found
+            throughout the country. The US has a great sense of adventure and a love for the
+            outdoors, making it the perfect place to enjoy a road trip. o drive one of our many
+            vehicles, you will need to be at least 21 years old and hold a standard driving license.
+            In case you are not a US citizen and you want to travel here, make sure to check which
+            type of visa you will be needing to enter the country.
+          </p>
+
+          <h2 className="heading-2 mt-3">Available for Rent Solis Air</h2>
+          <p className="body-1 leading-loose">
+            : 4 Seat campervan, which permits 4 people to sleep. It has an integrated bathroom with
+            a shower and toilet, an air conditioner, an integrated heating system, an integrated
+            kitchen with a sink, a flexible solar panel, a gas generator ($3.50/hour), and a
+            mosquito net. It includes a pop-top roof that functions as a sleeping area. The vehicle
+            is 21' feet long (6.40 m). Solis : 4 Seat campervan, which permits 4 people to sleep. It
+            has an integrated bathroom with a shower and toilet, an integrated heating system, an
+            integrated kitchen with a sink, a flexible solar panel, plus a gas generator
+            ($3.50/hour), and a mosquito net. It includes a pop-top roof that functions as a
+            sleeping area. The vehicle is 21' feet long (6.40 m). Tellaro : 4 seat campervan, which
+            permits 4 people to sleep. The campervan includes an integrated kitchen with a propane
+            stove, fridge, and freezer, an integrated bathroom with sink, a toilet with shower, air
+            conditioning, and a gas generator. The Tellaro also includes a pop-top roof that
+            functions as a sleeping area. This vehicle is 21' feet long (6.40 m). Metris : 4 Seat
+            campervan, which permits 4 people to sleep. It has an integrated induction stove,
+            mini-fridge (12 gallons), kitchen with sink, radio with GPS, AUX, and USB, exterior
+            shower, water tanks (clean water 6 gallons/grey water 6 gallons), and interior dining
+            table. The vehicle is 16'73" feet long (5 m). Wrangler : 5 Seat multi-purpose Jeep
+            vehicle which permits 2 people to sleep. It has a rooftop tent for sleeping, a foldable
+            staircase, a trail kitchen, a portable gas stove, an electric cooler, a solar shower, a
+            radio with AUX and USB, and a rear camera. The vehicle is 15'7" feet long (4.78 m).
+          </p>
+          <p className="body-1 leading-loose">
+            First, I visited Lago di Braies, the region’s most famous lake. Its scenic beauty and
+            the surrounding landscape make it a must-see. Due to limited parking for campervans, we
+            parked in the nearest village and took a 6€ bus to the lake. The walk around the lake
+            takes approximately an hour, so don’t miss the chance to have a picnic on its shores.
+            Afterward, we headed for the Rifugio Aronzo, located at an altitude of over 2000 meters.
+            The mountain hut is the starting point for two famous trails, the Cadini di Misurina, a
+            moderately easy walk with breathtaking views. And the Tre Cime de Lavaredo, one of the
+            postcards of the Dolomites, and a slightly more challenging trail. We did the Cadini di
+            Misurina trail in the late afternoon, which made us experience a wonderful sunset, and
+            then spent the night in the parking area ready for more hiking the next day!
+          </p>
+        </div>
+
+        <div>
+          <h2 className="heading-2 mt-3">Information and Requirements to Drive</h2>
+          <p className="body-1 leading-loose">
+            Although campervanning is popular throughout the world, nowhere is it more engrained in
+            the culture than in the United States! The US is the perfect place to rent a campervan
+            or RV and hit the road. The idea of the iconic American road trips still attracts people
+            from far and wide, all wanting to experience the splendor of its many landscapes.
+            Curiosity draws in local and international crowds to all the wonders that can be found
+            throughout the country. The US has a great sense of adventure and a love for the
+            outdoors, making it the perfect place to enjoy a road trip. o drive one of our many
+            vehicles, you will need to be at least 21 years old and hold a standard driving license.
+            In case you are not a US citizen and you want to travel here, make sure to check which
+            type of visa you will be needing to enter the country.
+          </p>
+
+          <h2 className="heading-2 mt-3">One-ways Available </h2>
+          <p className="body-1 leading-loose">
+            All the vehicles which are property of Indie Campers are covered by Basic Insurance, and
+            the type of coverage can vary according to state laws and country regulations. Please
+            make sure to check our Terms & Conditions according to the states you plan on visiting
+            along your road trip with us. All of our rental vehicles in North America have Automatic
+            Gear and have their steering wheels on the left side of the car. In the US, it is
+            mandatory to drive on the right side of the road. Our campervan and RV rentals include a
+            Kitchen kit, the vehicle's jack, triangle & vests, Basic Insurance, and 24/7 Assistance.{" "}
+          </p>
+
+          <h2 className="heading-2 mt-3">Cost-Effective Accommodation </h2>
+          <p className="body-1 leading-loose">
+            As the demand for hotels and popular vacation rental platforms surges, so do their
+            prices—often reaching a considerable premium, especially when near the Stadium. Sure,
+            having a room right next to the stadium sounds like a dream, but let’s talk reality: it
+            comes with a hefty price tag.
+          </p>
+
+          <h2 className="heading-2 mt-3">Freedom to Roam </h2>
+          <p className="body-1 leading-loose">
+            Traveling in a campervan unlocks the freedom to create your adventure. With Europe as
+            your playground during the Euro Cup, explore host cities, charming villages, and scenic
+            landscapes at your own pace. Whether it’s catching a match in a bustling metropolis or
+            unwinding in the tranquillity of nature, the choice is entirely yours. Also, the Euro
+            Cup will take place in one of Europe’s premier campervan destinations: Germany, known
+            for its camping culture, with its numerous campsites, well-maintained roads, and scenic
+            routes, the country boasts an impressive 16 national parks and 98 nature reserves to
+            explore. On our 5th day, we started by visiting Lago di Carezza, a stunning lake that
+            offers a short and easy walk around it. Next, we explored the vast meadow of Alpe di
+            Siusi or Seiser Alm, known for its incredible scenery and outdoor activities. A
+            curiosity: it’s a ski resort in winter and spring! To avoid the cable car, it’s
+            important to arrive before 9 am, as the car park is only available when the cable car
+            isn’t running. For an unforgettable experience, consider renting an electric bike,
+            although it comes at an expense, as it allows you to fully enjoy the beauty of Alpe di
+            Siusi.
+          </p>
+        </div>
       </div>
     </div>
   );
